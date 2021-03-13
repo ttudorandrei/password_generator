a4d740c4288ed5d127d5db03d6236a2ec07db707
Tudor Tocan <tudor.tocan@gmail.com>
2021-03-13 18:36:58 +0000

    add boilerplate code for generatePassword function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Declare a new function on line 3
+function generatePassword() {
+	// Create a variable to store our generated password
+	var password = ''
+
+	// Logic would go here
+	...
+
+	// Return our created password
+	return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
